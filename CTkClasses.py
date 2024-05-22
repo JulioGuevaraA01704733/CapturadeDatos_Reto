f7eced21374423460b97e59ca9cb398a56517b72
David-A-T <126364589+David-A-T@users.noreply.github.com>
2024-05-22 15:12:46 -0600

    Delete CTkClasses.py

--- CTkClasses.py
+++
@@ -1,6 +0,0 @@
-import customtkinter
-
-class ToplevelWindow(customtkinter.CTkToplevel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.geometry("400x300")
